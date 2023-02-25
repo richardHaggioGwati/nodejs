ecec47afd60999a04e47a4b9cd0025b91d552cb6
Richard Gwati <haggio0844@gmail.com>
2023-02-25 23:13:39 +0200

    feat: basics

---
+++ express_basics/controller/people.js
@@ -0,0 +1,80 @@
+let { people } = require('../../data');
+
+const getPeople = (req, res) => {
+	res.status(200).json({ success: true, data: people });
+};
+
+/**
+ *? working with js posting data => POST: Insert
+ *! if the proper http verb is not provided then the path  will fail
+ */
+const createPerson = (req, res) => {
+	const { name } = req.body;
+	if (!name) {
+		return res
+			.status(400)
+			.send({ success: false, message: 'please enter a name' });
+	}
+	res.status(201).json({ success: true, person: name });
+};
+
+/**
+ *? Api testing with Postman
+ */
+const createPersonPostman = (req, res) => {
+	const { name } = req.body;
+	if (!name) {
+		return res
+			.status(400)
+			.send({ success: false, message: 'please enter a name' });
+	}
+	res.status(201).json({ success: true, data: [...people, name] });
+};
+
+/**
+ * ? Put method => update
+ */
+const updatePerson = (req, res) => {
+	const { name } = req.body;
+	const { id } = req.params;
+
+	const person = people.find((person) => person.id === Number(id));
+
+	if (!person) {
+		return res
+			.status(404)
+			.json({ success: false, message: `No such person exit's` });
+	}
+
+	const newPeople = people.map((person) => {
+		if (person.id === Number(id)) {
+			person.name = name;
+		}
+		return person;
+	});
+	res.status(200).json({ success: true, data: newPeople });
+};
+
+/**
+ * ? Delete verb => remove 🤿
+ */
+const deletePerson = (req, res) => {
+	const id = req.params.id;
+
+	const person = people.find((person) => person.id === Number(id));
+
+	if (!person) {
+		return res.status(404).json({ success: false, message: 'Invalid user id' });
+	}
+
+	const newPeople = people.filter((person) => person.id !== Number(id));
+	res.status(200).json({ success: true, message: newPeople });
+};
+
+module.exports = {
+	getPeople,
+	createPerson,
+	createPersonPostman,
+	updatePerson,
+	deletePerson,
+};
--- express_basics/routes/people.js
+++ express_basics/routes/people.js
@@ -5,80 +5,22 @@
 const express = require('express');
 const router = express.Router();
 
-let { people } = require('../../data');
+const {
+	getPeople,
+	createPerson,
+	createPersonPostman,
+	updatePerson,
+	deletePerson,
+} = require('../controller/people');
 
+// router.get('/', getPeople);
+// router.post('/', createPerson);
+// router.post('/postman', createPersonPostman);
+// router.put('/:id', updatePerson);
+// router.delete('/:id', deletePerson);
 
-// default browser option => Get
-router.get('/', (req, res) => {
-	res.status(200).json({ success: true, data: people });
-});
+router.route('/').get(getPeople).post(createPerson);
+router.route('/postman').post(createPersonPostman);
+router.route('/:id').put(updatePerson).delete(deletePerson);
 
-/**
- *? working with js posting data => POST: Insert
- *! if the proper http verb is not provided then the path  will fail
- */
-router.post('/', (req, res) => {
-	const { name } = req.body;
-	if (!name) {
-		return res
-			.status(400)
-			.send({ success: false, message: 'please enter a name' });
-	}
-	res.status(201).json({ success: true, person: name });
-});
-
-/**
- *? Api testing with Postman
- */
-router.post('/postman', (req, res) => {
-	const { name } = req.body;
-	if (!name) {
-		return res
-			.status(400)
-			.send({ success: false, message: 'please enter a name' });
-	}
-	res.status(201).json({ success: true, data: [...people, name] });
-});
-
-/**
- * ? Put method => update
- */
-router.put('/:id', (req, res) => {
-	const { name } = req.body;
-	const { id } = req.params;
-
-	const person = people.find((person) => person.id === Number(id));
-
-	if (!person) {
-		return res
-			.status(404)
-			.json({ success: false, message: `No such person exit's` });
-	}
-
-	const newPeople = people.map((person) => {
-		if (person.id === Number(id)) {
-			person.name = name;
-		}
-		return person;
-	});
-	res.status(200).json({ success: true, data: newPeople });
-});
-
-/**
- * ? Delete verb => remove 🤿
- */
-router.delete('/:id', (req, res) => {
-	const id = req.params.id;
-
-	const person = people.find((person) => person.id === Number(id));
-
-	if (!person) {
-		return res.status(404).json({ success: false, message: 'Invalid user id' });
-	}
-
-	const newPeople = people.filter((person) => person.id !== Number(id));
-	res.status(200).json({ success: true, message: newPeople });
-});
-
-
-module.exports = router
+module.exports = router;
