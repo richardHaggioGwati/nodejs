24310e4aeb877d4ee27b641e02a9600260a3d8c3
richardHaggioGwati <haggiog0844@gmail.com>
2023-08-17 17:04:44 +0200

    register_beta

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -30,7 +30,7 @@ const start = async () => {
   try {
     await connectDb(process.env.MONGO_URI)
     app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+      console.log(`Server is listening on port ${port}....`)
     );
   } catch (error) {
     console.log(error);
