103a4205d38406a68bfc7a8afe1b8ec850f11141
richardHaggioGwati <haggiog0844@gmail.com>
2023-08-28 14:36:28 +0200

    user_authentication

--- 06-jobs-api/middleware/authentication.js
+++ 06-jobs-api/middleware/authentication.js
@@ -1,22 +1,22 @@
 const User = require('../models/User')
-const { UnauthenticatedError} = require('../errors')
 const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
 
 const auth = async (req, res, next) => {
     // check header
-    const authHeader = req.header.authorization
-    if (!authHeader || !authHeader.startsWith('Bearer')) throw new UnauthenticatedError('Authentication invalid')
-
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new UnauthenticatedError('Authentication invalid')
+    }
     const token = authHeader.split(' ')[1]
 
     try {
-        const payload = jwt.verify(token, 'JWT_SECRET')
-        // attach name to job routes if necessary
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        // attach the user to the job routes
         req.user = { userId: payload.userId, name: payload.name }
         next()
     } catch (error) {
-        console.log(error)
-        // throw new UnauthenticatedError('Authentication invalid')
+        throw new UnauthenticatedError('Authentication invalid')
     }
 }
 
--- 06-jobs-api/models/User.js
+++ 06-jobs-api/models/User.js
@@ -5,42 +5,44 @@ const jwt = require('jsonwebtoken')
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Please provide a valid name'],
-        minLength: 3,
-        maxLength: 50,
+        required: [true, 'Please provide name'],
+        maxlength: 50,
+        minlength: 3,
     },
     email: {
         type: String,
-        required: [true, 'Please provide a valid name'],
+        required: [true, 'Please provide email'],
         match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             'Please provide a valid email',
         ],
         unique: true,
     },
     password: {
         type: String,
-        required: [true, 'Please provide a password'],
-        minLength: 6,
-    }
+        required: [true, 'Please provide password'],
+        minlength: 6,
+    },
 })
 
-// pre is running before hash password is stored in the database
-UserSchema.pre('save', async function() {
+UserSchema.pre('save', async function () {
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
 })
 
-// Adding a function to the schema
-UserSchema.method.createJWT = function () {
-    return jwt.sign({userId: this.user._id, name: this.user.name}, 'JWT_SECRET', {
-        expiresIn: '30d'
-    })
+UserSchema.methods.createJWT = function () {
+    return jwt.sign(
+        { userId: this._id, name: this.name },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_LIFETIME,
+        }
+    )
 }
 
-// Validate Password
-UserSchema.method.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password)
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+    const isMatch = await bcrypt.compare(canditatePassword, this.password)
+    return isMatch
 }
 
 module.exports = mongoose.model('User', UserSchema)
