7132a05316a99febfcb28cde6d97d8ea4b07fa5d
richardHaggioGwati <haggiog0844@gmail.com>
2023-09-01 18:06:22 +0200

    controllers_getAllJobs&&createJob

--- 06-jobs-api/controllers/jobs.js
+++ 06-jobs-api/controllers/jobs.js
@@ -1,5 +1,10 @@
+const Jobs = require("../models/Job")
+const {StatusCodes} = require("http-status-codes")
+const {BadRequestError, UnauthenticatedError} = require("../errors")
+
 const getAllJobs = async (req, res) => {
-    res.send('Get all jobs')
+    const job = await Jobs.find({ createdBy: req.user.userId }).sort("createdAt")
+    res.status(StatusCodes.OK).json({ job, length: job.length })
 }
 
 const getJob = async (req, res) => {
@@ -7,7 +12,9 @@ const getJob = async (req, res) => {
 }
 
 const createJob = async (req, res) => {
-    res.json(req.user)
+    req.body.createdBy = req.user.userId
+    const job = await Jobs.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 const updateJob = async (req, res) => {
