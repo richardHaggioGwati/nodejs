d0ab364b2cd4632af7ea432b37a3fd8c00e9dc66
richardHaggioGwati <haggiog0844@gmail.com>
2023-09-01 18:46:05 +0200

    controllers_get_single_job

--- 06-jobs-api/controllers/jobs.js
+++ 06-jobs-api/controllers/jobs.js
@@ -1,20 +1,28 @@
 const Jobs = require("../models/Job")
 const {StatusCodes} = require("http-status-codes")
-const {BadRequestError, UnauthenticatedError} = require("../errors")
+const {BadRequestError, NotFoundError} = require("../errors")
 
 const getAllJobs = async (req, res) => {
-    const job = await Jobs.find({ createdBy: req.user.userId }).sort("createdAt")
-    res.status(StatusCodes.OK).json({ job, length: job.length })
+    const job = await Jobs.find({createdBy: req.user.userId}).sort("createdAt")
+    res.status(StatusCodes.OK).json({job, length: job.length})
 }
 
 const getJob = async (req, res) => {
-    res.send('Get job')
+    const {user: {userId}, params: {id: jobId}} = req
+
+    const job = await Jobs.findOne({
+        _id: jobId, createdBy: userId
+    })
+
+    if (!job) throw new NotFoundError(`No job with ${jobId}`)
+
+    res.status(StatusCodes.OK).json({job})
 }
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId
     const job = await Jobs.create(req.body)
-    res.status(StatusCodes.CREATED).json({ job })
+    res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJob = async (req, res) => {
